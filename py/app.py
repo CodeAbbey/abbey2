62d91d9c4a8ea82d38fbe9240d214abcb629b5c3
Rusty-Beard <tapinov.oleg@yandex.ru>
2018-10-05 07:44:09 +0300

    Custom 404 page, some minor improvements to tasks module (#5)

--- py/app.py
+++ py/app.py
@@ -4,6 +4,10 @@ from ctl.tasks import tasks_ctl
 app = flask.Flask(__name__, static_url_path = '/s')
 app.register_blueprint(tasks_ctl)
 
+@app.errorhandler(404)
+def page_not_found(e):
+  return flask.render_template('404.html'), 404
+
 @app.route('/')
 def main_page():
     return flask.render_template('index.html')
--- py/ctl/tasks.py
+++ py/ctl/tasks.py
@@ -2,7 +2,7 @@ import flask
 
 tasks_ctl = flask.Blueprint('tasks', __name__, url_prefix='/tasks')
 
-task_info = [['prg-1', 'Sample task 1'], ['prg-2', 'Sample task 2'], ['prg-3', 'Sample task 3']]
+task_info = {'prg-1': 'Sample task 1', 'prg-2': 'Sample task 2', 'prg-3':'Sample task 3'}
 
 @tasks_ctl.route('/')
 def task_list():
@@ -10,10 +10,8 @@ def task_list():
 
 @tasks_ctl.route('/<id>')
 def task_view(id):
-    found = None
-    for item in task_info:
-        if item[0] == id:
-            found = item
-            break
-    return flask.render_template('tasks/view.html', data = found)
+    if id in task_info:
+        return flask.render_template('tasks/view.html', data = [id,task_info[id]])
+    else:
+        flask.abort(404)
 
