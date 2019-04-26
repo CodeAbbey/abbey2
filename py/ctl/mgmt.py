793397575954f40ec7df7bfc9b60300683b5cd2a
Rodion Gorkovenko <rodiongork@github.com>
2019-04-26 14:47:59 +0300

    Editing blobs from mgmt

--- py/ctl/mgmt.py
+++ py/ctl/mgmt.py
@@ -1,5 +1,7 @@
 import flask
 
+import dao.utils
+
 mgmt_ctl = flask.Blueprint('mgmt', __name__)
 
 
@@ -18,3 +20,23 @@ def mgmt_main():
 @mgmt_ctl.route('/blobs')
 def mgmt_blobs():
     return flask.render_template('mgmt/blobs.html')
+
+
+@mgmt_ctl.route('/blobs/<id>', methods=['POST'])
+def blob_store(id):
+    body = flask.request.get_data(cache=False)
+    update_blob(id, body)
+    return 'Ok\r\n'
+
+
+def update_blob(id, body):
+    dao.utils.update_blob(id, body, 'blobs')
+
+
+@mgmt_ctl.route('/blobs/<id>', methods=['GET'])
+def blob_view(id):
+    body = dao.utils.query_one('blobs', 'id=%s', (id,), 'val')
+    if body is None:
+        return ('', 204)
+    else:
+        return body[0].decode('utf-8')
--- py/ctl/tools.py
+++ py/ctl/tools.py
@@ -1,4 +1,3 @@
-import hashlib
 import json
 import base64
 import re
@@ -15,36 +14,6 @@ tools_ctl = flask.Blueprint('tools', __name__)
 def action_log():
     data = dao.users.action_log_recent(100)
     return flask.render_template('mess.html', data=data)
-
-
-@tools_ctl.route('/blobs/<id>', methods=['POST'])
-def blob_store(id):
-    body = flask.request.get_data(cache=False)
-    verify = flask.request.headers.get('X-Hash', '')
-    body_hash = blob_digest(body)
-    if body_hash != verify:
-        return 'Method Not Allowed\r\n', 405
-    update_blob(id, body)
-    return 'Ok\r\n'
-
-
-def update_blob(id, body):
-    dao.utils.update_blob(id, body, 'blobs')
-
-
-@tools_ctl.route('/blobs/<id>', methods=['GET'])
-def blob_view(id):
-    verify = flask.request.headers.get('X-Hash', '')
-    id_hash = blob_digest(id.encode('utf-8'))
-    if id_hash != verify:
-        return 'Method not Allowed\r\n', 405
-    body = dao.utils.query_one('blobs', 'id=%s', (id,), 'val')
-    return ':)\r\n' if body is None else body[0].decode('utf-8')
-
-
-def blob_digest(binary):
-    salt = flask.current_app.config['BLOB_SALT'].encode('utf-8')
-    return hashlib.sha512(salt + binary).hexdigest()
 
 
 @tools_ctl.route('/update_stats', methods=['GET'])
